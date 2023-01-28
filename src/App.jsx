4c688c904b6e6d2935748bbedfef985799e8bc3e
namhels <8.lsv.888@gmail.com>
2023-01-28 20:24:44 +0300

    add hooks

--- src/App.jsx
+++ src/App.jsx
@@ -1,60 +1,72 @@
-import React, { Component } from 'react';
+import { useState } from 'react';
+import Box from 'components/Box';
 import Section from "components/Section";
 import FeedbackOptions from "components/FeedbackOptions";
 import Statistics from "components/Statistics";
-// import Notification from "components/Notification";
-import Notification from 'components/Notification/Notification ';
-import Box from 'components/Box';
+import Notification from "components/Notification";
 
-class App extends Component {
-  state = {
-    good: 0,
-    neutral: 0,
-    bad: 0,
-  };
-// 8888888888888888
-  handleLeaveFeedback = (e) => {
-    const option = e.target.dataset.label;
-    this.setState(prevState => ({
-      [option]: prevState[option] + 1,
-    }));
-  };
-
-  countTotalFeedback = () => {
-    return Object.values(this.state).reduce((acc, el) => acc + el, 0);
-  };
-
-  countPositiveFeedbackPercentage = () =>
-    `${Math.round(
-      (this.state.good / this.countTotalFeedback()) * 100
-    )}%`;
-
-  render() {
-    const total = this.countTotalFeedback();
-    const stats = [...Object.entries(this.state), ["Total", total], ["Positive feedback", this.countPositiveFeedbackPercentage()]];
-    return (
-      <Box
-        p={5}
-        bg="backgroundPrimary"
-      >
-        <Section title="Please leave feedback💹">
-          <FeedbackOptions
-            options={Object.keys(this.state)}
-            onLeaveFeedback = {this.handleLeaveFeedback}
-          />
-        </Section>
-        <Section title="📊Statistics📈">
-          {this.countTotalFeedback()
-            ? <Statistics
-              stats={stats}/>
-            : <Notification message="There is no feedback" />
-          }
-        </Section>
-      </Box>
-    )
-  };
+const INITIAL = {
+  GOOD: 'Good',
+  NEUTRAL: 'Neutral',
+  BAD: 'Bad',
+  TOTAL: 'Total',
+  POSITIVE_FEEDBACK: 'Positive feedback',
 };
 
-export default App;
+export const App = () => {
+  const [good, setGood] = useState(0);
+  const [neutral, setNeutral] = useState(0);
+  const [bad, setBad] = useState(0);
+
+  const handleLeaveFeedback = (e) => {
+    const option = e.target.dataset.label;
+    switch (option) {
+      case INITIAL.GOOD:
+        setGood(prevState => prevState + 1);
+        break;
+      case INITIAL.NEUTRAL:
+        setNeutral(prevState => prevState + 1);
+        break;
+      case INITIAL.BAD:
+        setBad(prevState => prevState + 1);
+        break;
+      default:
+        break;
+    }
+  };
+
+  const countTotalFeedback = () => {
+    return [good, neutral, bad].reduce((acc, el) => acc + el, 0);
+  };
+
+  const countPositiveFeedbackPercentage = () =>
+    `${Math.round(
+      (good / countTotalFeedback()) * 100
+    )}%`;
+
+  const stats = [[INITIAL.GOOD, good], [INITIAL.NEUTRAL, neutral], [INITIAL.BAD, bad], [INITIAL.TOTAL, countTotalFeedback()], [INITIAL.POSITIVE_FEEDBACK, countPositiveFeedbackPercentage()]];
+
+  return (
+    <Box
+      p={5}
+      bg="backgroundPrimary"
+    >
+      <Section title="Please leave feedback💹">
+        <FeedbackOptions
+          options={[INITIAL.GOOD, INITIAL.NEUTRAL, INITIAL.BAD]}
+          onLeaveFeedback={handleLeaveFeedback}
+        />
+      </Section>
+      <Section title="📊Statistics📈">
+        {countTotalFeedback()
+          ? <Statistics
+            stats={stats} />
+          : <Notification message="There is no feedback" />
+        }
+      </Section>
+    </Box>
+  );
+};
+
 
 
